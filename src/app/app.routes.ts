a1f841bab7f0fbd61188e952c80f982ec803dfd3
matraore <matraore@engeem.com>
2024-10-12 20:20:48 +0000

    page double-factor

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { DashboardComponent } from './views/dashboard/dashboard.component';
 import { AppointmentsComponent } from './views/appointments/appointments.component';
 import { CarnetComponent } from './views/carnet/carnet.component';
 import { HomeDashboardComponent } from './views/home-dashboard/home-dashboard.component';
+import { DoubleFactorComponent } from './views/auth/double-factor/double-factor.component';
 
 export const routes: Routes = [
 
@@ -15,7 +16,12 @@ export const routes: Routes = [
         path: "auth",
         children: [
             { path: "login", component: LoginComponent },
-            { path: "register", component: RegisterComponent }
+            { path: "register", component: RegisterComponent },
+            { path: "double-factor", component: DoubleFactorComponent },
+
+            
+
+
         ]
     },
 
